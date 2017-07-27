9a3d32adccfea6292218d68dbde81267adad5e52
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-07-27 18:14:34 +0200

    Update scripts

--- grid/upload_custom_libs.py
+++ grid/upload_custom_libs.py
@@ -10,23 +10,26 @@ libs = ['TrackCutChecker/lib/libTrackCutChecker.so']
 print('Packing custom libraries in tar:', libs)
 
 dirName = 'lib/marlin_dll'
-rootDirName = dirName.split('/')[0]
+mainDirName = dirName.split('/')[0]
 eosName = '/ilc/user/a/amaier/mylibs.tar.gz'
 
 libs = map(lambda a: basePath + a, libs)
 
-os.system('cp -r ' + rootDirName + ' $TRASH/')
-os.system('rm -rf ' + rootDirName)
+
+os.system('set -x')
+os.system('cp -r ' + mainDirName + ' $TRASH/')
+os.system('rm -rf ' + mainDirName)
 os.system('mkdir -p ' + dirName)
 
 for lib in libs:
 	os.system('cp ' + lib + ' ' + dirName)
 
-os.system('tar -zcvf ' + rootDirName + '.tar.gz ' + rootDirName + '/')
+os.system('tar -zcvf ' + mainDirName + '.tar.gz ' + mainDirName + '/')
 
 
-os.system('dirac-dms-add-file ' + eosName + ' ' + rootDirName + '.tar.gz CERN-DST-EOS')
-os.system('mv ' + rootDirName + '.tar.gz lib_mv.tar.gz')
+os.system('dirac-dms-remove-files ' + eosName)
+os.system('dirac-dms-add-file ' + eosName + ' ' + mainDirName + '.tar.gz CERN-DST-EOS')
+os.system('mv ' + mainDirName + '.tar.gz lib_mv.tar.gz')
 os.system('dirac-dms-replicate-lfn ' + eosName + ' RAL-SRM')
 os.system('dirac-dms-replicate-lfn ' + eosName + ' DESY-SRM')
 os.system('dirac-dms-replicate-lfn ' + eosName + ' CERN-DIP-4')
