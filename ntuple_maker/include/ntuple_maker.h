e332b96b4aa4e674bfc3b6550daaeb1e5013f188
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-07-28 14:27:50 +0200

    Rework processor to write out correct branches

--- ntuple_maker/include/ntuple_maker.h
+++ ntuple_maker/include/ntuple_maker.h
@@ -1,21 +1,21 @@
 #ifndef NTUPLE_MAKER_H
 #define NTUPLE_MAKER_H
-
-#include "marlin/Processor.h"
-#include "lcio.h"
 #include <string>
 #include <vector>
+
 #include "TH1F.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TLorentzVector.h"
 
+#include "lcio.h"
+#include "marlin/Processor.h"
+#include "marlin/VerbosityLevels.h"
 #include <EVENT/LCCollection.h>
 #include <EVENT/MCParticle.h>
 #include <EVENT/Track.h>
 #include <EVENT/ReconstructedParticle.h>
 #include <EVENT/ParticleID.h>
-
 #include <UTIL/LCRelationNavigator.h>
 
 using namespace lcio ;
@@ -67,63 +67,24 @@ class ntuple_maker : public Processor {
  protected:
 
   //parameters
-  std::string m_treeName;
   std::string m_outfileName;
 
   //collections
-  std::string m_inputParticleCollection ;
-  std::string m_inputTrackCollection ;
-  std::string m_inputTrackRelationCollection ;
-  std::string m_outputPandoraPFOCollection ;
-  std::string m_inputPFORelationCollection ;
-  std::string m_outputJetkt7 ;
+  std::string m_IsolatedLepton_collection ;
+  std::string m_durham_4jets_collection ;
+  std::string m_kt_4jets_radius05_collection ;
+  std::string m_kt_4jets_radius07_collection ;
+  std::string m_kt_4jets_radius10_collection ;
+  std::string m_kt_4jets_radius12_collection ;
+  std::string m_kt_4jets_radius15_collection ;
+  std::vector<std::string> inputCollections;
 
-  TTree *tracktree ;
-  TFile *trackfile ;
+  TTree *rawTree ;
+  TFile *ntupleFile ;
 
   //histos
-  TH1F* h_res_mc_pfo_etot;
-  TH1F* h_res_mc_jet_etot;
-  TH1F* h_dR_jet_pfo;
 
   //branches
-  TLorentzVector mc_particle;
-  int mc_n;
-  double mc_etot;
-  std::vector<int> mc_pdg;
-  std::vector<double> mc_pt;
-  std::vector<double> mc_theta;
-  std::vector<double> mc_phi;
-  std::vector<double> mc_e;
-
-  TLorentzVector trk_particle;
-  int trk_n;
-  double trk_etot;
-  std::vector<int> trk_pdg;
-  std::vector<double> trk_pt;
-  std::vector<double> trk_theta;
-  std::vector<double> trk_phi;
-  std::vector<double> trk_e;
-  std::vector<int> trk_hit_n;
-  std::vector<double> trk_chi2;
-  std::vector<double> trk_chi2ndf;
-  std::vector<int> trk_ndf;
-
-  TLorentzVector pfo_fourvec;
-  int pfo_n;
-  double pfo_etot;
-  std::vector<int> pfo_pdg;
-  std::vector<double> pfo_pt;
-  std::vector<double> pfo_theta;
-  std::vector<double> pfo_phi;
-  std::vector<double> pfo_e;
-  std::vector<int> pfo_trk_n;
-  std::vector<int> pfo_trk_relation;
-  std::vector<int> pfo_trk_pdg;
-  std::vector<double> pfo_trk_phi;
-  std::vector<double> pfo_trk_omega;
-  std::vector<double> pfo_trk_dedx;
-
   TLorentzVector jet_fourvec;
   int jet_n;
   double jet_etot;
@@ -131,7 +92,6 @@ class ntuple_maker : public Processor {
   std::vector<double> jet_theta;
   std::vector<double> jet_phi;
   std::vector<double> jet_e;
-  std::vector<std::vector<double>> jet_pfo_relation;
 
   int _nRun ;
   int _nEvt ;
