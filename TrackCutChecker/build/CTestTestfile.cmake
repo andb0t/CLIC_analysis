c81251822ab2a123d9af239b1d0b3c7fd5e0ab6c
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-07-28 11:02:44 +0200

    Remove build and lib dirs from repo

--- TrackCutChecker/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/cern.ch/user/a/amaier/CLIC/TrackCutChecker
-# Build directory: /afs/cern.ch/user/a/amaier/CLIC/TrackCutChecker/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
