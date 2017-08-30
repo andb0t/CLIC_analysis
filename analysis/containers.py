978bea02125d733f07245c0dcf0defbc0a1ba0ea
Andreas Alexander Maier <andbot@gmx.de>
2017-08-30 18:26:12 +0200

    First implementation of _mean option

--- analysis/containers.py
+++ analysis/containers.py
@@ -2,16 +2,21 @@ import re
 import itertools
 
 import pandas as pd
+import numpy as np
 
 
 class physics_container:
 
-    def __init__(self, inputFile, maxEvt):
+    def __init__(self, inputFile, maxEvt, verbose=0):
         print('Initializing physics object from file', inputFile, 'using', maxEvt, 'events')
         self._data = pd.read_csv(inputFile, sep="\t", comment="#", index_col=False, engine="python",
                                  header=0, nrows=maxEvt, na_values='-')
         self._names = list(self._data.dtypes.index)[:-1]
         self._namesIter = 0
+        self._verbose = verbose
+        if self._verbose > 1:
+            print('Loaded those data:')
+            print(self._data)
 
     def show(self):
         print('Data loaded:', self._names)
@@ -21,20 +26,42 @@ class physics_container:
         # any alphanumeric char '\w_n'
         # all but one char 'lep_pt_[^4]'
         p = re.compile(regex)
-        return list(filter(lambda x: re.search(p, x), self._names))
+        filteredNames = list(filter(lambda x: re.search(p, x), self._names))
+        if filteredNames:
+            return filteredNames
+        else:
+            if self._verbose:
+                print('No name with regex "' + str(regex) + '" found. Return regex!')
+            return [regex]
 
     def get(self, name=''):
         if name:
-            return getattr(self._data, name).dropna()
+            if name in self._names:
+                return getattr(self._data, name)
+            else:
+                if self._verbose:
+                    print('"' + str(name) + '" not in names list. Apply defined functions!')
+                if name.endswith('_mean'):
+                    newRegex = re.sub('\_mean$', '', name)
+                    print('Calculate mean of names corresponding to regex', newRegex, 'for each event!')
+                    obsLists = list(self.get_list(regex=newRegex))
+                    mean = [np.nanmean(x) for x in zip(*obsLists)]
+                    mean = list(map(lambda x: x if x == x else 0, mean))
+                    print('mean:', mean)
+                    return mean
+                else:
+                    print('Error: neither found regex nor corresponding defined function to name' +
+                          str(name) + '. Return None!')
+                    return None
         else:
             try:
-                return getattr(self._data, self.names[self._namesIter]).dropna()
+                return getattr(self._data, self.names[self._namesIter])
             finally:
                 self._namesIter += 1
                 self._namesIter %= len(self.names)
 
     def get_chained(self, regex=''):
-        return list(itertools.chain.from_iterable([self.get(name).dropna() for name in self.names(regex)]))
+        return itertools.chain.from_iterable([self.get(name) for name in self.names(regex)])
 
     def get_list(self, regex=''):
-        return [self.get(name).dropna() for name in self.names(regex)]
+        return [self.get(name) for name in self.names(regex)]
--- analysis/plot_data.py
+++ analysis/plot_data.py
@@ -6,7 +6,7 @@ import containers
 import styles
 
 
-MAX_EVT = 1000
+MAX_EVT = 3
 # MAX_EVT = None
 
 plt.rcParams['figure.figsize'] = (8.0, 5.0)
@@ -16,13 +16,14 @@ get_ipython().magic('matplotlib inline')
 
 importlib.reload(containers)
 fname = "all_output_small.txt"
-physCont = containers.physics_container(fname, MAX_EVT)
-physCont.show()
+physCont = containers.physics_container(fname, MAX_EVT, verbose=1)
+# physCont.show()
 
 
 def plot_raw(regex='', save=None):
     fig, ax = plt.subplots()
     for name in physCont.names(regex):
+        print(physCont.get(name))
         ax.plot(physCont.get(name), label=name)
     ax.set(ylabel='Value', xlabel='Event')
     styles.style(ax)
@@ -46,6 +47,8 @@ def plot_hist(regex='', xRange=None, nBins=30, stacked=False, chained=False, sav
         fig.savefig(save)
 
 
+plot_hist('jet_DH_pt_mean')
+plot_raw('lep_n')
 plot_raw('\w*_n')
 plot_hist('\w*_n', (0, 10), 10)
 plot_hist('lep_pt', (0, 200))
