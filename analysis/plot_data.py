9c3d099f425e62bdb18986db73fd33a9ccb10a7e
Andreas Alexander Maier <andbot@gmx.de>
2017-08-30 21:28:41 +0200

    Improve plot style

--- analysis/plot_data.py
+++ analysis/plot_data.py
@@ -6,7 +6,7 @@ import containers
 import styles
 
 
-MAX_EVT = 5
+MAX_EVT = 100
 # MAX_EVT = None
 
 plt.rcParams['figure.figsize'] = (8.0, 5.0)
@@ -16,16 +16,16 @@ get_ipython().magic('matplotlib inline')
 
 importlib.reload(containers)
 fname = "all_output_small.txt"
-physCont = containers.physics_container(fname, MAX_EVT, verbose=2)
+physCont = containers.physics_container(fname, MAX_EVT, verbose=1)
 # physCont.show()
 
 
 def plot_raw(regex='', save=None):
     fig, ax = plt.subplots()
     for name in physCont.names(regex):
-        ax.plot(physCont.get(name), label=name)
+        ax.plot(physCont.get(name), label=name, marker='.')
     ax.set(ylabel='Value', xlabel='Event')
-    styles.style(ax)
+    styles.style_raw(ax)
     if save:
         fig.savefig(save)
 
@@ -41,13 +41,15 @@ def plot_hist(regex='', xRange=None, nBins=30, stacked=False, chained=False, sav
         for name in physCont.names(regex):
             ax.hist(physCont.get(name), nBins, normed=1, range=xRange, label=name, stacked=False)
     ax.set(ylabel='Entries', xlabel='Value')
-    styles.style(ax)
+    styles.style_hist(ax)
     if save:
         fig.savefig(save)
 
 
+plot_raw('_n')
+plot_raw('lep_pt')
 plot_raw('lep_pt_mean')
-plot_raw('lep_n')
+plot_hist('lep_pt_mean')
 plot_raw('\w*_n')
 plot_hist('\w*_n', (0, 10), 10)
 plot_hist('lep_pt', (0, 300))
--- analysis/styles.py
+++ analysis/styles.py
@@ -1,4 +1,9 @@
-def style(axis):
+def style_hist(axis):
     axis.grid(True, which='both')
     axis.legend(loc="best")
     axis.margins(x=0)
+
+
+def style_raw(axis):
+    axis.grid(True, which='both')
+    axis.legend(loc="best")
