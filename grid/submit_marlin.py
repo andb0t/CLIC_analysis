410527d133903e18f80919c9fee355428f2817ce
Andreas Alexander Maier <andbot@gmx.de>
2017-10-03 15:20:30 +0200

    Replace tabs with spaces

--- grid/submit_marlin.py
+++ grid/submit_marlin.py
@@ -4,14 +4,12 @@ from __future__ import print_function
 import argparse
 import os
 import os.path
-import sys
-
-from DIRAC.Core.Base import Script
-Script.parseCommandLine()
 
 from ILCDIRAC.Interfaces.API.DiracILC import DiracILC
 from ILCDIRAC.Interfaces.API.NewInterface.UserJob import UserJob
 from ILCDIRAC.Interfaces.API.NewInterface.Applications import Marlin
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
 
 
 MAX_N_FILES = -1
@@ -31,113 +29,115 @@ print('Submitting jobs based on:', args.input)
 
 
 def get_job_name():
-	return 'output_' + os.path.basename(args.input.rstrip('.txt').rstrip('.slcio'))
+    return 'output_' + os.path.basename(args.input.rstrip('.txt').rstrip('.slcio'))
 
 
 def get_input_files():
-	inputDataList = []
+    inputDataList = []
 
-	if args.input.endswith('.slcio'):
-		inputDataList = ['LFN:' + args.input]
-	else:
-		allFilesList = []
-		with open(args.input) as inputDatFile:
-			for index, line in enumerate(inputDatFile):
-				line = line.strip()
-				if line.startswith('#'): 
-					continue
-				if index > MAX_N_FILES and MAX_N_FILES != -1:
-					break
-				allFilesList.append('LFN:' + line)
+    if args.input.endswith('.slcio'):
+        inputDataList = ['LFN:' + args.input]
+    else:
+        allFilesList = []
+        with open(args.input) as inputDatFile:
+            for index, line in enumerate(inputDatFile):
+                line = line.strip()
+                if line.startswith('#'):
+                    continue
+                if index > MAX_N_FILES and MAX_N_FILES != -1:
+                    break
+                allFilesList.append('LFN:' + line)
 
-		for i in xrange(0, len(allFilesList), BATCH_SIZE):
-		    batch = allFilesList[i:i+BATCH_SIZE]
-		    inputDataList.append(batch)
+        for i in xrange(0, len(allFilesList), BATCH_SIZE):
+            batch = allFilesList[i:i+BATCH_SIZE]
+            inputDataList.append(batch)
 
-		print('Total of ' + str(len(allFilesList)) + ' files! Splitting it in ' + str(len(inputDataList)) + ' batches with length ' + str(BATCH_SIZE) )
+        print('Total of ' + str(len(allFilesList)) +
+              ' files! Splitting it in ' + str(len(inputDataList)) +
+              ' batches with length ' + str(BATCH_SIZE))
 
-	return inputDataList
+    return inputDataList
 
 
 def remove_file(path, file):
-	global replaceAll
-	fullpath = STORAGE_USER_PATH + path + '/' + file
-	if replaceAll:
-		os.system('dirac-dms-remove-files ' +fullpath )
-		return False
-		
-	print('Warning! ' + fullpath + ' exists! Delete this? y/[n]/all')
-	deleteIt = raw_input()
+    global replaceAll
+    fullpath = STORAGE_USER_PATH + path + '/' + file
+    if replaceAll:
+        os.system('dirac-dms-remove-files ' + fullpath)
+        return False
 
-	if deleteIt == 'y' or deleteIt == 'all':
-		os.system('dirac-dms-remove-files ' +fullpath )
-		if deleteIt == 'all':
-			replaceAll = True
-		return False
-	else:
-		print('Aborting!')
-		return True
+    print('Warning! ' + fullpath + ' exists! Delete this? y/[n]/all')
+    deleteIt = raw_input()
+
+    if deleteIt == 'y' or deleteIt == 'all':
+        os.system('dirac-dms-remove-files ' + fullpath)
+        if deleteIt == 'all':
+            replaceAll = True
+        return False
+    else:
+        print('Aborting!')
+        return True
 
 
 def check_file_existence(path, file):
-	fullpath = STORAGE_BASE_PATH + STORAGE_USER_PATH + path + '/' + file
-	if os.path.isfile(fullpath):
-		return remove_file(path, file)
-	else:
-		return False
+    fullpath = STORAGE_BASE_PATH + STORAGE_USER_PATH + path + '/' + file
+    if os.path.isfile(fullpath):
+        return remove_file(path, file)
+    else:
+        return False
 
 
 def create_job(inputData, saveName, dontPromptMe):
 
-	outputPath = 'files/'+ get_job_name()
-	slcioFile = saveName + '.slcio'
-	rootFile = saveName + '.root'
+    outputPath = 'files/' + get_job_name()
+    slcioFile = saveName + '.slcio'
+    rootFile = saveName + '.root'
 
-	if check_file_existence(outputPath, slcioFile): return True
-	if check_file_existence(outputPath, rootFile): return True
+    if check_file_existence(outputPath, slcioFile):
+        return True
+    if check_file_existence(outputPath, rootFile):
+        return True
 
-	dIlc = DiracILC()
+    dIlc = DiracILC()
 
-	job = UserJob()
-	job.setOutputSandbox(['*.out', '*.log', '*.sh', '*.py', '*.xml'])
-	if SAVE_SLCIO: 
-		job.setOutputData([slcioFile, rootFile], OutputPath=outputPath, OutputSE=STORAGE_SE)
-	else:
-		job.setOutputData(rootFile, OutputPath=outputPath, OutputSE=STORAGE_SE)
-	job.setJobGroup( 'myMarlinRun1' )
-	job.setName( 'MyMarlinJob1' )
-	# job.setBannedSites(['LCG.IN2P3-CC.fr','OSG.UConn.us','LCG.Cracow.pl','OSG.MIT.us','LCG.Glasgow.uk','OSG.CIT.us','OSG.BNL.us','LCG.Brunel.uk'])
-	job.setInputSandbox( ['LFN:/ilc/user/a/amaier/mylibs.tar.gz'] )
+    job = UserJob()
+    job.setOutputSandbox(['*.out', '*.log', '*.sh', '*.py', '*.xml'])
+    if SAVE_SLCIO:
+        job.setOutputData([slcioFile, rootFile], OutputPath=outputPath, OutputSE=STORAGE_SE)
+    else:
+        job.setOutputData(rootFile, OutputPath=outputPath, OutputSE=STORAGE_SE)
+    job.setJobGroup('myMarlinRun1')
+    job.setName('MyMarlinJob1')
+    # job.setBannedSites(['LCG.IN2P3-CC.fr','OSG.UConn.us','LCG.Cracow.pl','OSG.MIT.us','LCG.Glasgow.uk','OSG.CIT.us','OSG.BNL.us','LCG.Brunel.uk'])
+    job.setInputSandbox(['LFN:/ilc/user/a/amaier/mylibs.tar.gz'])
 
-	marl = Marlin ()
-	marl.setVersion('ILCSoft-2017-05-30_gcc62')
+    marl = Marlin()
+    marl.setVersion('ILCSoft-2017-05-30_gcc62')
 
-	marl.setInputFile(inputData)
-	# marl.setInputFile(['LFN:/ilc/prod/clic/1.4tev/qq_ln/ILD/DST/00003249/010/qq_ln_dst_3249_10000.slcio'])
-	marl.setSteeringFile('job_files/full_ntuple_maker.xml')
-	marl.setOutputFile(slcioFile)
-	marl.setGearFile('/afs/cern.ch/user/a/amaier/CLIC/grid/job_files/clic_ild_cdr.gear')
-	marl.setExtraCLIArguments("--my_ntuple_maker.OutputFileName={rootOutfile}".format(rootOutfile = rootFile))
-	marl.setNumberOfEvents(1000)
+    marl.setInputFile(inputData)
+    # marl.setInputFile(['LFN:/ilc/prod/clic/1.4tev/qq_ln/ILD/DST/00003249/010/qq_ln_dst_3249_10000.slcio'])
+    marl.setSteeringFile('job_files/full_ntuple_maker.xml')
+    marl.setOutputFile(slcioFile)
+    marl.setGearFile('/afs/cern.ch/user/a/amaier/CLIC/grid/job_files/clic_ild_cdr.gear')
+    marl.setExtraCLIArguments("--my_ntuple_maker.OutputFileName={rootOutfile}".format(rootOutfile=rootFile))
+    marl.setNumberOfEvents(1000)
 
-	job.append(marl)
-	if dontPromptMe: 
-		job.dontPromptMe()
-	job.submit(dIlc)
+    job.append(marl)
+    if dontPromptMe:
+        job.dontPromptMe()
+    job.submit(dIlc)
 
-	return False
+    return False
 
 
 replaceAll = False
 dontPromptMe = False
 for index, inputData in enumerate(get_input_files()):
-	if index == 1:
-		print("Don't prompt and accept all following submissions? y/[n]")
-		inp =  raw_input()
-		if inp == 'y':
-			dontPromptMe = True 
-	saveName = get_job_name() + '_batch_' + str(index)
-	if create_job(inputData, saveName, dontPromptMe):
-		break
-
-
+    if index == 1:
+        print("Don't prompt and accept all following submissions? y/[n]")
+        inp = raw_input()
+        if inp == 'y':
+            dontPromptMe = True
+    saveName = get_job_name() + '_batch_' + str(index)
+    if create_job(inputData, saveName, dontPromptMe):
+        break
--- grid/submit_marlin_all.py
+++ grid/submit_marlin_all.py
@@ -1,11 +1,15 @@
 #!/bin/env python
 from __future__ import print_function
 
+import os
 import subprocess
 
 
-command = 'submit_marlin.py'
+DATA_DIR = 'file_lists'
+BASE_CMD = 'python submit_marlin.py'
 
-print(command)
+for datafile in os.listdir(DATA_DIR):
+    print(BASE_CMD + ' ' + datafile)
+
 
 # subprocess.call([command], shell=True)
