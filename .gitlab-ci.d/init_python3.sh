cc2658787060af948af04990c3c9c82b38a94366
Andreas Alexander Maier <andbot@gmx.de>
2017-10-06 00:09:05 +0200

    Update ci

--- .gitlab-ci.d/init_python3.sh
+++ .gitlab-ci.d/init_python3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "Setting python3 environment..."
 export PYTHONDIR=/cvmfs/clicdp.cern.ch/software/Python/3.5.2/x86_64-slc6-gcc61-opt
 export PATH=$PYTHONDIR/bin:$PATH
 export LD_LIBRARY_PATH=$PYTHONDIR/lib:$LD_LIBRARY_PATH
