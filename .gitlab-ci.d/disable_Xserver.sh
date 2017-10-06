097220b4449ead698716f7624683b1f296f154c7
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-06 11:46:23 +0200

    Update CI

--- .gitlab-ci.d/disable_Xserver.sh
+++ .gitlab-ci.d/disable_Xserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Disabling Xserver..."
-echo "line backend : Agg" >> /root/.local/lib/python3.5/site-packages/matplotlib/mpl-data/matplotlibrc
+echo "backend : Agg" >> /root/.local/lib/python3.5/site-packages/matplotlib/mpl-data/matplotlibrc
 
 
