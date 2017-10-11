975409f4b5d7b49d8e3d66eb2e7cd6ec573d24aa
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-11 11:41:13 +0200

    Add offset to copied lines

--- analysis/example_data/update_example_data.sh
+++ analysis/example_data/update_example_data.sh
@@ -2,6 +2,7 @@
 DATA_DIR="/afs/cern.ch/work/a/amaier/CLIC/csv"
 THIS_DIR="$(dirname "$0")"
 N_EVENTS=5000
+N_EVENTS_OFFSET=1
 
 echo "Updating example data from $DATA_DIR"
 ls $DATA_DIR
@@ -10,9 +11,10 @@ ls $DATA_DIR
 declare -a arr=("3246" "3249" "5572" "2717" "2094" "2163" "2152" "2169")
 
 ## now loop through the above array
+N_LINES=$((N_EVENTS + N_EVENTS_OFFSET))
 for i in "${arr[@]}"
 do
 	FILE_NAME="$DATA_DIR/$i.csv"
-	echo "Copying $N_EVENTS events to $FILE_NAME"
-	head -n $N_EVENTS "$FILE_NAME" > "$THIS_DIR"/"$i".csv
+	echo "Copying $N_LINES events to $FILE_NAME"
+	head -n $N_LINES "$FILE_NAME" > "$THIS_DIR"/"$i".csv
 done
