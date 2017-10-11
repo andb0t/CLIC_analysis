ffe85c497085e9fa5d445fffce1ca6c3deff34be
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-11 16:03:31 +0200

    Rename and check for .tex files as well

--- doc/extract_custom_includes.py
+++ doc/extract_custom_includes.py
@@ -6,10 +6,11 @@ import os
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("file", help='LateX .dep file with figures to be extracted')
+parser.add_argument("file", default='presentation.dep', help='LateX .dep file with figures to be extracted')
 args = parser.parse_args()
 
-KEYS = ['.pdf', '.png', '.eps', '.jpg']
+KEYS = ['.pdf', '.png', '.eps', '.jpg', '.tex']
+MUST = ['../']
 
 
 def find_between( s, first, last ):
@@ -24,8 +25,9 @@ def find_between( s, first, last ):
 with open(args.file, 'r') as file:
 	for line in file:
 		if any(key in line for key in KEYS):
-			# print(line)
-			line = line.rstrip().lstrip().lstrip('*{file}')
-			resource = find_between(line, '{', '}{')
-			print(resource)
-			# os.system('ls {0}'.format(resource))
+			if any(must in line for must in MUST):
+				# print(line.rstrip())
+				line = line.rstrip().lstrip().lstrip('*{file}')
+				resource = find_between(line, '{', '}{')
+				print(resource)
+				# os.system('ls {0}'.format(resource))
