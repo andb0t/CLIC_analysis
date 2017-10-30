b2ab0f33b2e605a7b496210dafaabdfcac9b6b9f
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-30 16:54:19 +0100

    Test nan in CI tables

--- analysis/src/form/yields.py
+++ analysis/src/form/yields.py
@@ -1,3 +1,4 @@
+import math
 import re
 import tabulate
 
@@ -18,7 +19,7 @@ def print_event_yields(dataCont, name='event', latex=False, silent=False):
         events = cont.df[settings.SF].sum()
         print(cont.df[settings.SF])
         print('None possible:', events)
-        events = 0 if events is None else events
+        events = 0 if math.isnan(events) else events
         print('Note None:', events)
         table.append([cont.name, '{:d}'.format(entries), '{:.1f}'.format(events)])
         totEntries += entries
