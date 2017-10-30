5bf76a0311d7c4da7b551f15d806d71096f381ba
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-10-30 13:59:51 +0100

    Update CI

--- .gitlab-ci.d/copy_full_analysis_files.sh
+++ .gitlab-ci.d/copy_full_analysis_files.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-REMOTEPATH=$1
-KRB_USERNAME=$2
+KRB_USERNAME=$1
+REMOTEPATH=/afs/cern.ch/work/a/amaier/CLIC/csv/
 
 echo "Copying full data files from $REMOTEPATH"
 
 ssh $KRB_USERNAME@lxplus "ls $REMOTEPATH"
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/3249.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/3246.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/5572.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/2094.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/4034.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/5527.csv analysis/example_data/
+scp ${KRB_USERNAME}@lxplus:$REMOTEPATH/5594.csv analysis/example_data/
