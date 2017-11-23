0c890fc76b5a9f904d011881aa702f27c0212fb8
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-11-23 13:48:46 +0100

    Add script to update external MarlinReco repo

---
+++ processors/ElectronDresser/README.md
@@ -0,0 +1,7 @@
+# ElectronDresser
+
+## Author
+Andreas A. Maier <andreas.alexander.maier@cern.ch>
+
+## Description
+To be added
---
+++ processors/update_MarlinReco.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Update MarlinReco ..."
+cp -r ElectronDresser/include ../../MarlinReco/Analysis/ElectronDresser
+cp -r ElectronDresser/src ../../MarlinReco/Analysis/ElectronDresser
+cp -r ElectronDresser/README.md ../../MarlinReco/Analysis/ElectronDresser
