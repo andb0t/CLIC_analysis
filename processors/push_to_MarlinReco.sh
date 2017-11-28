e7400635541b2ad06d7fd64c0ad423242f3a5f2a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-11-28 16:41:43 +0100

    Rename script

--- processors/push_to_MarlinReco.sh
+++ processors/push_to_MarlinReco.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Update MarlinReco ..."
+echo "Push to MarlinReco ..."
 cp IsolatedLeptonFinder/include/*.h ../../MarlinReco/Analysis/IsolatedLeptonFinder/include
 cp IsolatedLeptonFinder/src/*.cc ../../MarlinReco/Analysis/IsolatedLeptonFinder/src
 cp IsolatedLeptonFinder/README ../../MarlinReco/Analysis/IsolatedLeptonFinder
