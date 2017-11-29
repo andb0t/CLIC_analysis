ce0df414426a6a5bb292c86409ac8d0a449c2c6e
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2017-11-29 18:13:05 +0100

    Remove IsolatedLeptonFinder

--- processors/push_to_MarlinReco.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Push to MarlinReco ..."
-cp IsolatedLeptonFinder/include/*.h ../../MarlinReco/Analysis/IsolatedLeptonFinder/include
-cp IsolatedLeptonFinder/src/*.cc ../../MarlinReco/Analysis/IsolatedLeptonFinder/src
-cp IsolatedLeptonFinder/README ../../MarlinReco/Analysis/IsolatedLeptonFinder
