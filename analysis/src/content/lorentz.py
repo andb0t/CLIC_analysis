52724f17298946b127e4df849e5a1b7d7998ee31
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-29 13:51:22 +0200

    Add lorentz subtraction and __str__

--- analysis/src/content/lorentz.py
+++ analysis/src/content/lorentz.py
@@ -5,8 +5,13 @@ from src.utils import utils
 
 # maybe have to implement px, py, pz, m etc as functions to avoid memory usage
 
-
 class lorentz:
+    """ A class for Lorentz vectors
+
+    Keyword arguments:
+    coords -- coordinate system for initialization
+    """
+
     def __init__(self, a, b, c, d, coords='PtThetaPhiE'):
         if coords == 'PtThetaPhiE':
             self.pt = a
@@ -39,9 +44,19 @@ class lorentz:
         dotProd = np.absolute(physics.dot(self.vec, self.vec))
         self.m = np.sqrt(dotProd)
 
+    def __str__(self):
+        return 'lorentz({:.2f}, {:.2f}, {:.2f}, {:.2f}, coords=\'PtThetaPhiE\')'.format(self.pt, self.theta, self.phi, self.e)
+
+    def __neg__(self):
+        return lorentz(-self.px, -self.py, -self.pz, self.e, coords='PxPyPzE')
+
     def __add__(self, other):
         px = np.add(self.px, other.px)
         py = np.add(self.py, other.py)
         pz = np.add(self.pz, other.pz)
         e = np.add(self.e, other.e)
         return lorentz(px, py, pz, e, coords='PxPyPzE')
+
+    def __sub__(self, other):
+        return self + -other
+
