90404bd7a9b7ac11a1cff4eea210b12db41cbf5f
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-04-03 17:12:38 +0200

    Fix some sig digits

--- analysis/src/form/yields.py
+++ analysis/src/form/yields.py
@@ -78,7 +78,7 @@ def print_samples(conts, latex=False, name=''):
         return int(re.search(r'\d+', str).group())
 
     headers = ['Sample', 'ID', 'Xsec [fb]']
-    table = [[cont.original['name'], extract_int(cont.fileName), cont.xSec * cont.df.shape[0] / cont.original['size']] for cont in conts]
+    table = [[cont.original['name'], extract_int(cont.fileName), round(cont.xSec * cont.df.shape[0] / cont.original['size'], 1)] for cont in conts]
     print(tabulate.tabulate(table, headers=headers, tablefmt='grid'))
     if latex:
         if name:
